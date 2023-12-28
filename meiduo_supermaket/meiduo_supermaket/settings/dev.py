5af4d820813d1d03efc7507e886832d11b5f27c4
hanjoy1 <2754436372@qq.com>
2023-12-28 10:36:09 +0800

    配置静态文件
    初步配置完成

--- meiduo_supermaket/meiduo_supermaket/settings/dev.py
+++ meiduo_supermaket/meiduo_supermaket/settings/dev.py
@@ -164,7 +164,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# 静态文件配置
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
